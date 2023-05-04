005dcb4fd5550337e19f4e058505e982b09aa832
fanyingjie <fanyingjie@fang.com>
2023-05-04 23:42:45 +0800

    初始化仓库

---
+++ .vscode/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello c++" << endl;
+    cin.get();
+
+bool checkInclusion(string t,string s){
+    unordered_map<char,int> need,window;
+
+}
+
+}
+
+
+
+
